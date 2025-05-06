42ef9e644baca6e3b8e9762836e508c353994725
Azello-XD <jonathanlenathea@gmail.com>
2025-05-07 03:40:16 +0700

    Update config.py

--- userbot/config.py
+++ userbot/config.py
@@ -1,26 +1,26 @@
 import os
 
 DEVS = [
-    5646661182
+    1927018403
 ]
 
-API_ID = int(os.getenv("API_ID", "22146045"))
+API_ID = int(os.getenv("API_ID", "27418440"))
 
-API_HASH = os.getenv("API_HASH", "80807ed4e66a8d60d30645a390a35163")
+API_HASH = os.getenv("API_HASH", "0a08a360e0e9f41b9896f655c300d09d")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7609025339:AAEvlo_3Zj8XuZtWgKPfzs6awMTZsD8Wvrg")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8123679703:AAHnB7LpUtfEKvYQdJslVzb5kq61IlKMHII")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "5646661182"))
+OWNER_ID = int(os.getenv("OWNER_ID", "1927018403"))
 
-USER_ID = list(map(int,os.getenv("USER_ID", "5646661182",).split(),))
+USER_ID = list(map(int,os.getenv("USER_ID", "1927018403",).split(),))
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002486020552"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002547370737"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002486020552").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002547370737").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 
 COMMAND = os.getenv("COMMAND", ".")
 PREFIX = COMMAND.split()
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://yunadallenbot:asmaralokamujomok12321@cluster0.j3dr1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Kinaya:KontolXD#123@kinaya.yuf2elr.mongodb.net/?retryWrites=true&w=majority&appName=Kinaya")
